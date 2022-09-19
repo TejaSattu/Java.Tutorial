4811069e67b31eff225349c256c46734983ee98a
Teja Sattu <tejasattu2@gmail.com>
2022-09-19 16:59:52 +0530

    Java Basic program

---
+++ src/Tutorialjava.java
@@ -0,0 +1,10 @@
+public class Tutorialjava {
+    public static void main(String[] args){
+        String a = "hello world!";
+        System.out.println(a);
+        float q = 5;
+        float m = 4;
+        System.out.println(q+m);
+
+    }
+}
