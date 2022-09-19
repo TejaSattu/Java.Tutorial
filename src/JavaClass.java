6e36a2564cc0faf73144d4ed88333de5318bd47d
Teja Sattu <tejasattu2@gmail.com>
2022-09-19 16:00:20 +0530

    Java Tutorial

---
+++ src/JavaClass.java
@@ -0,0 +1,10 @@
+public class JavaClass {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        System.out.println("The sum of:"+ (a+b));
+        System.out.println("The sub of:"+ (a-b));
+        System.out.println("The multiple of:"+ (a*b));
+        System.out.println("The sum of:"+(a%b));
+    }
+}
